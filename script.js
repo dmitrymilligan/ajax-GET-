adcc07fa96cfe8b21329e55fcace1bd74ed0b916
dmitrymilligan <milligan@inbox.ru>
2016-01-27 15:14:02 +0200

    Ajax GET second

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 $(function () {
 
-$(document).on("submit", "form", function () {
-var name  	= $("input[name='name']").val(),
-		phone 	= $("input[name='phone']").val(),
-  	car     = $("select[name='car']").val(),
-    pattern = /[0-9]{3}/;
-if(pattern.test(phone)) {
-submitForm(name, phone, car);
-}
-else {
-alert(false);
-};
-});
+	$(document).on("submit", "form", function () {
+		var name  	= $("input[name='name']").val(),
+			phone 	= $("input[name='phone']").val(),
+			car     = $("select[name='car']").val(),
+			pattern = /[0-9]{3}/;
+		if(pattern.test(phone)) {
+			submitForm(name, phone, car);
+		}
+		else {
+			alert(false);
+		};
+	});
 
-function submitForm(name, phone, car) {
-	var root = 'http://jsonplaceholder.typicode.com';
-$.ajax({
-method: "GET",
-url: root + '/posts/1',
-data: { Name: name, Phone: phone, Car: car }
-}).fail(function () {
-	alert("FAIL")
-}).done(function (data) {
-	alert(data);
-});
-}
+	function submitForm(name, phone, car) {
+		var root = 'http://jsonplaceholder.typicode.com';
+		$.ajax({
+			method: "GET",
+			url: root + '/posts/1',
+			data: { Name: name, Phone: phone, Car: car }
+		}).fail(function () {
+			alert("FAIL")
+		}).done(function (data) {
+			alert(data);
+		});
+	}
 });
