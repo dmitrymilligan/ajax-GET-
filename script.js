1cb0ff2882bd17e9db3020c786517103a2dcbbbf
Dmitry <milligan@inbox.ru>
2016-01-28 11:15:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,18 +12,28 @@ $(function () {
 			alert(false);
 		};
 	});
-
-	function submitForm(name, phone, car) {
-		var root = 'http://jsonplaceholder.typicode.com';
-		$.ajax({
-			method: "GET",
-			url: root + '/posts/1',
-			data: { Name: name, Phone: phone, Car: car }
-		}).fail(function () {
-			alert("FAIL")
-		}).done(function (data) {
-			for(var a in data)
-	 		$(".one").append(data[a] + ": " + data + "<br>" );
-	 		});
+	function submitForm() {
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+				document.getElementsByClassName("one")[0].innerHTML = xhttp.responseText;
+			}
+		};
+		xhttp.open("GET", "http://jsonplaceholder.typicode.com", true);
+		xhttp.send();
 	}
+
+//	function submitForm(name, phone, car) {
+//		var root = 'http://jsonplaceholder.typicode.com';
+//		$.ajax({
+	// 		method: "GET",
+	// 		url: root + '/posts/1',
+	// 		data: { Name: name, Phone: phone, Car: car }
+	// 	}).fail(function () {
+	// 		alert("FAIL")
+	// 	}).done(function (data) {
+	// 		for(var a in data)
+	//  		$(".one").append(data[a] + ": " + data + "<br>" );
+	//  		});
+	// }
 });
